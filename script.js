c83c5316f02bab8f437f7fcee80a60ba7bd1a550
Ono <emmanuel.ishaka@yahoo.com>
2024-11-11 21:30:08 +0100

    Succesfully made COmputer choice function for rock/paper/scissors

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello World')
+
+
+function getComputerChoice(rand) {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        return "Rock";
+    }
+    else if (randomNumber === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice())
