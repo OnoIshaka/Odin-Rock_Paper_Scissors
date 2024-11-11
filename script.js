6c96a35669a7891fc316dee7bfa130fffb649929
Ono <emmanuel.ishaka@yahoo.com>
2024-11-11 22:36:40 +0100

    Added the logic of rock/paper/scissors

--- script.js
+++ script.js
@@ -14,4 +14,54 @@ function getComputerChoice(rand) {
     }
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper or Scissors: ");
+    return choice;
+}
+
+// console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    computerChoice = computerChoice.toLowerCase()
+
+    if (humanChoice == computerChoice){
+        console.log("It's a Tie")
+    }
+    
+    else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log('Paper beats Rock')
+        humanScore++;
+    }
+    else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        console.log('Paper beats Rock')
+        computerScore++;
+    }
+    else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        console.log('Scissors beats Paper')
+        computerScore++;
+    }
+    else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        console.log('Scissors beats Paper')
+        humanScore++;
+    }
+    else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log('Rock beats Scissors')
+        humanScore++;
+    }
+    else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        console.log('Rock beats Scissors')
+        computerScore++;
+    }
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+  
